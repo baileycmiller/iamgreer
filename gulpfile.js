13bf098be66479b35285628f1c5864f41c14545c
Bailey Miller <bailey@gaslight.co>
2017-05-28 16:29:11 -0400

    Add dependencies

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+/**METHODS
+
+gulp.task(name, fn) – registers a function with a name
+gulp.watch(glob, fn) – runs a function when a file that matches the glob changes
+gulp.src(glob) – returns a readable stream
+gulp.dest(folder) – returns a writable stream
+
+**/
+
+//not sure, was in tutorial
+'use strict';
+
+// Include gulp, sass, and un-css
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//Sass task
+gulp.task('sass', function(){
+  return gulp.src('scss/main.scss') //the source sass file
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('css')) //the folder you want the css to go
+}); //run 'gulp sass' to compile
+
+//Watch for changes in sass to update css automatically
+gulp.task('automate', function() {
+    gulp.watch('scss/**/*.scss', ['sass']);
+}); //run 'gulp automate'
